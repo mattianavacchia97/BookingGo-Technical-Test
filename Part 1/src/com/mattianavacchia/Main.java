6845d8f4075434d46c0f1a793630055b3ebc358c
mattianavacchia <mattia.navacchia.97@gmail.com>
2020-02-19 09:09:02 +0000

    Add scanner for pickup and dropoff location

--- Part 1/src/com/mattianavacchia/Main.java
+++ Part 1/src/com/mattianavacchia/Main.java
@@ -1,8 +1,26 @@
 package com.mattianavacchia;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        String supplier = "", pickup = "", dropoff = "";
+
+        if (args.length == 0) {
+            System.out.println("Provide name of rider. (Dave, Eric, Jeff)");
+            System.exit(0);
+        } else {
+            supplier = args[0].toLowerCase();
+        }
+
+        // pickup
+        System.out.print("Enter pickup location in the format longitude, latitude (i.e. 51.470020,-0.454295) : ");
+        Scanner scanner = new Scanner(System. in);
+        pickup = scanner. nextLine();
+
+        // dropoff
+        System.out.print("Enter dropoff location in the format longitude, latitude (i.e. 3.410632,-2.157533) : ");
+        dropoff = scanner. nextLine();
     }
 }
