b90c5669300c583d88d20e3eedbcc854d035e18f
mattianavacchia <mattia.navacchia.97@gmail.com>
2020-02-21 15:32:16 +0000

    Implementation call for all suppliers

--- Part 1/src/com/mattianavacchia/AllSupplier.java
+++ Part 1/src/com/mattianavacchia/AllSupplier.java
@@ -1,71 +1,92 @@
 package com.mattianavacchia;
 
+import javafx.util.Pair;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.util.Collections;
+import java.net.*;
 import java.util.HashMap;
 import java.util.Map;
 
 class AllSupplier {
-    final int READ_TIMEOUT = 2000;
-    final String[] supplier = new String[] {"dave", "eric", "jeff"};
-    private final Map<String, Integer> carTypePassengers;
-    {
-        Map<String, Integer> aMap = new HashMap<>();
-        aMap.put("STANDARD", 4);
-        aMap.put("EXECUTIVE", 4);
-        aMap.put("LUXURY", 4);
-        aMap.put("PEOPLE_CARRIER", 6);
-        aMap.put("LUXURY_PEOPLE_CARRIER", 6);
-        aMap.put("MINIBUS", 16);
-        carTypePassengers = Collections.unmodifiableMap(aMap);
-    }
+    private static final int READ_TIMEOUT = 2000;
+    private static final String[] supplier = new String[] {"Dave", "Eric", "Jeff"};
+    private static Map<String, Integer> carTypePassengers =
+            Map.of("STANDARD", 4, "EXECUTIVE", 4, "LUXURY", 4, "PEOPLE_CARRIER", 6, "LUXURY_PEOPLE_CARRIER", 6, "MINIBUS", 16);
+    private static int passengers = 0;
+    private static Map<String, Pair<String, Integer>> map = new HashMap<>();
+    private static int currentSupplier = 0;
 
-    void getAllSupplierResult(final String urlApi) throws IOException {
-        for (String s: supplier) {
-            parseJSON(getJSONFromUrl(urlApi));
+    static void getAllSupplierResult(final String urlApi, final String pickup, final String dropoff, final int numberOfPassengers) {
+        for (int i = 0; i < supplier.length; i++) {
+            currentSupplier = i;
+            passengers = numberOfPassengers;
+            parseJSON(getJSONFromUrl(urlApi + supplier[i] + "?pickup=" + pickup + "&dropoff=" + dropoff, i));
         }
+
+        if (map.size() == 0)
+            System.out.println("No cars available.");
+        else
+            for(Map.Entry<String, Pair<String, Integer>> entry : map.entrySet())
+                System.out.println("{" + entry.getKey() + "} - {" + entry.getValue().getKey() + "} - {" + entry.getValue().getValue() + "}");
     }
 
-    void parseJSON(final JSONObject jsonObject) {
+    private static void parseJSON(final JSONObject jsonObject) {
         if (jsonObject != null) {
+            JSONArray options = jsonObject.getJSONArray("options");
+            for(Object o: options)
+                if ( o instanceof JSONObject ) {
+                    String car_type = (String) ((JSONObject) o).get("car_type");
+                    int price = ((JSONObject) o).getInt("price");
 
-        } else {
-            System.out.println("Error");
+                    if (carTypePassengers.containsKey(car_type)
+                        && carTypePassengers.get(car_type) >= passengers) {
+                        if (map.containsKey(car_type)) {
+                            if (map.get(car_type).getValue() > price)
+                                map.replace(car_type, new Pair<>(supplier[currentSupplier], price));
+                        } else
+                            map.put(car_type, new Pair<>(supplier[currentSupplier], price));
+                    }
+                }
         }
     }
 
-    JSONObject getJSONFromUrl(final String url) throws IOException {
-        URL urlForGetRequest = new URL(url);
-        String readLine = null;
+    private static JSONObject getJSONFromUrl(final String url, final int i) {
+        try {
+            URL urlForGetRequest = new URL(url);
+            String readLine = null;
 
-        HttpURLConnection connection = (HttpURLConnection) urlForGetRequest.openConnection();
-        connection.setRequestMethod("GET");
-        connection.setRequestProperty("Content-Type", "application/json");
-        //connection.setConnectTimeout(5000);
-        connection.setReadTimeout(READ_TIMEOUT);
+            HttpURLConnection connection = (HttpURLConnection) urlForGetRequest.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Content-Type", "application/json");
+            //connection.setConnectTimeout(5000);
+            connection.setReadTimeout(READ_TIMEOUT);
 
-        int responseCode = connection.getResponseCode();
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream()));
-            StringBuffer response = new StringBuffer();
-            while ((readLine = in .readLine()) != null) {
-                response.append(readLine);
-            } in .close();
-            // print result
-            System.out.println("JSON String Result " + response.toString());
-            return new JSONObject(response);
-            //GetAndPost.POSTRequest(response.toString());
-        } else {
-            System.out.println("GET NOT WORKED");
-            return null;
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                while ((readLine = in .readLine()) != null) {
+                    response.append(readLine);
+                } in .close();
+                // print result
+                System.out.println("JSON String Result " + response.toString());
+                return new JSONObject(response.toString());
+                //GetAndPost.POSTRequest(response.toString());
+            } else {
+                System.out.println("HTTP connection to " + supplier[i] + " did not work.");
+                return null;
+            }
+        } catch (IOException e) {
+            if (e instanceof SocketTimeoutException) {
+                System.out.println(supplier[i] + " is not taken into consideration due to lack of response.");
+                return null;
+            }
         }
+        return null;
     }
 }
