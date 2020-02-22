6253623f23e1121f5253205fc5657ba57fc35c26
mattianavacchia <mattia.navacchia.97@gmail.com>
2020-02-22 08:17:09 +0000

    Update main class with new call to APIRequest class

--- Part 1/src/com/mattianavacchia/Main.java
+++ Part 1/src/com/mattianavacchia/Main.java
@@ -8,7 +8,7 @@ import static spark.Spark.get;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         String supplier = "", pickup = "", dropoff = "", urlApi = "https://techtest.rideways.com/";
         int numberOfPassengers = 0;
@@ -23,27 +23,16 @@ public class Main {
             dropoff = args[2];
         }
 
-        String finalPickup = pickup;
-        String finalDropoff = dropoff;
-
         if (supplier.contains("dave")) {
             if (args.length == 4) {
-                String finalSupplier = supplier;
-                get("/dave", (req, res)-> {
-                    res.type("application/json");
-                    return MapToJSON.jsonDave(Dave.getDaveResults(urlApi + finalSupplier + "?pickup=" + finalPickup + "&dropoff=" + finalDropoff));
-                });
+                APIRequest.callAPIDave(urlApi + supplier + "?pickup=" + pickup + "&dropoff=" + dropoff);
             } else
                 Dave.getDaveResults(urlApi + supplier + "?pickup=" + pickup + "&dropoff=" + dropoff);
         } else {
             numberOfPassengers = Integer.parseInt(args[3]);
-            int finalNumberOfPassengers = numberOfPassengers;
 
             if (args.length == 5) {
-                get("/all", (req, res)-> {
-                    res.type("application/json");
-                    return MapToJSON.jsonAll(AllSupplier.getAllSupplierResult(urlApi, finalPickup, finalDropoff, finalNumberOfPassengers));
-                });
+                APIRequest.callAPIAll(urlApi, pickup, dropoff, numberOfPassengers);
             } else {
                 AllSupplier.getAllSupplierResult(urlApi, pickup, dropoff, numberOfPassengers);
             }
